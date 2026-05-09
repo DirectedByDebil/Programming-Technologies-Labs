3dfbc4d62977e38dac052ae2c95e100b743797c8
Cooler Arsenii <chromov-arsenij@mail.ru>
2026-05-09 15:39:40 +0300

    added windows support for lab 2

---
+++ build_lab2.bat
@@ -0,0 +1,6 @@
+cls
+make -f Makefile_lab2_windows clean
+make -f Makefile_lab2_windows
+set OUTPUT="laba2_results.csv"
+.\laba_2 -mo 5 -f %OUTPUT%
+.\core\view\.venv\Scripts\python.exe .\core\view\plot_csv.py %OUTPUT%
